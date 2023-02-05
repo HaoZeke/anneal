b8412301ea2ca9c273e6cf5fcc0d011645b115e2
Rohit Goswami <rog32@hi.is>
2023-02-05 00:09:00 +0000

    ENH: Generalize the Styblinski-Tang

---
+++ anneal/funcs/objNd.py
@@ -0,0 +1,26 @@
+from anneal.core.components import NumLimit, ObjectiveFunction
+
+import numpy as np
+
+
+class StybTangNd(ObjectiveFunction):
+    """This is a generalization as the function is evaluated on a hypercube"""
+
+    def __init__(self, dims):
+        self.dims = dims
+        self.limits = NumLimit(
+            dims=self.dims,
+            low=np.ones(self.dims) * -5,
+            high=np.ones(self.dims) * 5,
+        )
+        self.glob_minma_val = -39.16599 * self.dims
+        self.glob_minma_pos = np.array([-2.903534] * self.dims)
+
+    def singlepoint(self, pos):
+        self.limits.check(pos)
+        return np.sum((pos**4) - (16 * (pos**2)) + (5 * pos)) / 2
+
+    def multipoint(self, pos):
+        return np.apply_along_axis(
+            self.singlepoint, 1, pos.reshape(self.limits.dims, -1)
+        )
