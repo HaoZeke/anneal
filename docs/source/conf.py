d848eab39fae80d26a1c8940aeb309ff3eff36da
Rohit Goswami <rog32@hi.is>
2024-02-17 00:33:52 +0000

    MAINT: Update the versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@
 project = "anneal"
 copyright = "2023--present, anneal Developers"
 author = "anneal Developers"
-version = "0.0.1"
+version = "0.0.8"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
