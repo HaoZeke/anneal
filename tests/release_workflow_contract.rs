de7bb1eb1d2acc840367d490068fa5bc7937f967
Rohit Goswami <rgoswami@ieee.org>
2026-08-20 07:30:14 -0500

    fix: consume self-contained xts header

--- tests/release_workflow_contract.rs
+++ tests/release_workflow_contract.rs
@@ -13,7 +13,7 @@ fn crate_publication_has_a_registry_fallback_for_git_dependencies() {
 
     assert!(manifest.contains("rgpot-core = { version = \"=3.0.2\", git = \"https://github.com/OmniPotentRPC/rgpot.git\", rev = \"a2ee5197a81eb9fcba57c6c6b25ed0a6f56e8056\""));
     assert!(manifest.contains("eindir-core = { git = \"https://github.com/HaoZeke/eindir.git\", rev = \"513b8b7fdfaf63cca0b94b2f3316cf2c19a8daaa\", version = \"0.6.0\", features = [\"capi\"]"));
-    assert!(manifest.contains("xtsci-optimize = { git = \"https://github.com/HaoZeke/xtsci-optimize.git\", rev = \"2c22d0619cba31026d6f7fa1e4518bb1522417d4\""));
+    assert!(manifest.contains("xtsci-optimize = { git = \"https://github.com/HaoZeke/xtsci-optimize.git\", rev = \"d96686d\""));
     assert!(workflow.contains("cargo publish --dry-run"));
     assert!(!workflow.contains("cargo publish --dry-run --no-verify"));
 }
