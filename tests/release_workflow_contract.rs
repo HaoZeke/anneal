8746d282a2d1042168c27d67ab79ffa37970f2b7
Rohit Goswami <rgoswami@ieee.org>
2026-08-20 09:54:23 -0500

    chore: refresh xtsci dependency pin

--- tests/release_workflow_contract.rs
+++ tests/release_workflow_contract.rs
@@ -13,7 +13,7 @@ fn crate_publication_has_a_registry_fallback_for_git_dependencies() {
 
     assert!(manifest.contains("rgpot-core = { version = \"=3.0.2\", git = \"https://github.com/HaoZeke/potlib.git\", rev = \"65d5488f202e0c7b1b3aeddeb388b3d3ba707fcf\""));
     assert!(manifest.contains("eindir-core = { git = \"https://github.com/HaoZeke/eindir.git\", rev = \"091c6f7d6ea70821c3374152481fb9500e799af2\", version = \"0.6.0\", features = [\"capi\"]"));
-    assert!(manifest.contains("xtsci-optimize = { git = \"https://github.com/HaoZeke/xtsci-optimize.git\", rev = \"5aeb315af819bce40ec56f9e76b862fab64447fc\""));
+    assert!(manifest.contains("xtsci-optimize = { git = \"https://github.com/HaoZeke/xtsci-optimize.git\", rev = \"d784953dfe7e2ef75f1dbe20130459e12436dcfd\""));
     assert!(workflow.contains("cargo publish --dry-run"));
     assert!(!workflow.contains("cargo publish --dry-run --no-verify"));
 }
