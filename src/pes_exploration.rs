d98087f8d02ebd6919132f292747ae393d7f58e0
Rohit Goswami <rgoswami@ieee.org>
2026-09-02 06:45:15 -0500

    test(pes): specify selectable IRC backend

--- src/pes_exploration.rs
+++ src/pes_exploration.rs
@@ -9,17 +9,17 @@ use std::fmt::{Display, Formatter};
 use std::sync::Mutex;
 
 use ndarray::{Array1, Array2, ArrayView1, ArrayView2};
-use rand::{rngs::StdRng, SeedableRng};
+use rand::{SeedableRng, rngs::StdRng};
 use rand_distr::{Distribution, StandardNormal};
 use rgmin::{
-    lowest_mode, ApplyHessian, Control, EigenParams, EigensolverKind, FireKind, GradNorm, Lbfgs,
-    Method, Oracle, Solver,
+    ApplyHessian, Control, EigenParams, EigensolverKind, FireKind, GradNorm, Lbfgs, Method, Oracle,
+    Solver, lowest_mode,
 };
 use rgsaddle::geom::update_trust;
 use rgsaddle::{
-    exact_eigh, prfo_trust_region, update_h, HessUpdate, IrcConfig, IrcDirection, IrcSession,
-    MinModeConfig, MinModeKind, MinModeSession, MinModeStatus, PointSurface, SaddleError,
-    TrustSchedule,
+    HessUpdate, IrcConfig, IrcDirection, IrcSession, MinModeConfig, MinModeKind, MinModeSession,
+    MinModeStatus, PointSurface, SaddleError, TrustSchedule, exact_eigh, prfo_trust_region,
+    update_h,
 };
 
 use crate::curvature::{project_rigid_with, rigid_basis};
@@ -50,14 +50,25 @@ pub trait PesSurface: Sync {
 mod tests {
     use std::sync::atomic::{AtomicUsize, Ordering};
 
-    use ndarray::{array, Array1, Array2, ArrayView1};
+    use ndarray::{Array1, Array2, ArrayView1, array};
 
     use super::{
-        activation_basis, deflate_cartesian_mode, finest_irc_step, home_uphill_mode,
-        refine_cartesian_with_prfo, roll_branch, IrcDirection, PesExplorationConfig, PesSurface,
+        IrcDirection, PesExplorationConfig, PesSurface, activation_basis, deflate_cartesian_mode,
+        finest_irc_step, home_uphill_mode, refine_cartesian_with_prfo, roll_branch,
     };
     use crate::curvature::project_rigid_with;
     use crate::descriptor_space::DescriptorGeometry;
+    use rgsaddle::IrcKind;
+
+    #[test]
+    fn irc_configuration_preserves_the_selected_rgsaddle_backend() {
+        let config = PesExplorationConfig {
+            irc_kind: IrcKind::Morokuma,
+            ..PesExplorationConfig::default()
+        };
+
+        assert_eq!(super::irc_config(&config, 0.125).kind, IrcKind::Morokuma);
+    }
 
     struct FailingIrcSurface;
 
